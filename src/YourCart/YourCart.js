afd67e9274aee06d0a7129b152d9d7af29bc4777
Gavin Grant <gavingrant@gmail.com>
2020-07-09 13:02:53 -0700

    progress version to debug

--- src/YourCart/YourCart.js
+++ src/YourCart/YourCart.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
-import './SumTotal';
 import SumTotal from './SumTotal';
+import Summary from './Summary';
 
 class YourCart extends Component {
     render() {
-        const { selected } = this.props;
-        
-        const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD'
-        });
-
-        const summary = Object.keys(selected).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
-            const selectedOption = selected[feature];
-      
-            return (
-                <div className="summary__option" key={featureHash}>
-                    <div className="summary__option__label">{feature}</div>
-                    <div className="summary__option__value">{selectedOption.name}</div>
-                    <div className="summary__option__cost">
-                        {USCurrencyFormat.format(selectedOption.cost)}
-                    </div>
-                </div>
-            );
-          });
-        
         return (
             <section className="main__summary">
                 <h2>Your cart</h2>
-                    {summary}
+                <Summary selected={this.props.selected} />
                 <SumTotal selected={this.props.selected} />
             </section>
         );
