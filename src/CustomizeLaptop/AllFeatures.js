50c4153f90a2261dcf2f8ea17ab0de0f98438af7
Gavin Grant <gavingrant@gmail.com>
2020-07-09 16:28:56 -0700

    working version with FeatureOptions component

--- src/CustomizeLaptop/AllFeatures.js
+++ src/CustomizeLaptop/AllFeatures.js
@@ -3,23 +3,12 @@ import FeatureOptions from './FeatureOptions'
 
 class AllFeatures extends Component {
     render() {
-        const allFeatures = Object.keys(this.props.features).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
-      
-            return (
-                <fieldset className="feature" key={featureHash}>
-                    <legend className="feature__name">
-                        <h3>{feature}</h3>
-                    </legend>
-                    <FeatureOptions 
-                        features={this.props.features}
-                        selected={this.props.selected}
-                        handleSelection={this.props.handleSelection}/>
-                </fieldset>
-            );
-        });
-        
-        return allFeatures
+        return (
+            <FeatureOptions 
+                features={this.props.features}
+                selected={this.props.selected}
+                handleSelection={this.props.handleSelection}/>
+        );
     }
 };
 
--- src/CustomizeLaptop/FeatureOptions.js
+++ src/CustomizeLaptop/FeatureOptions.js
@@ -8,27 +8,35 @@ class FeatureOptions extends Component {
             currency: 'USD'
         });
         
-        const allFeatures = Object.keys(this.props.features).map(feature => {
+        const allFeatures = Object.keys(this.props.features).map((feature, idx) => {
             const { selected } = this.props;
+            const featureHash = feature + '-' + idx;
             const options = this.props.features[feature].map(item => {
                 const itemHash = slugify(JSON.stringify(item));
                 return (
                     <div key={itemHash} className="feature__item">
-                        <input
-                            type="radio"
-                            id={itemHash}
-                            className="feature__option"
-                            name={slugify(feature)}
-                            checked={item.name === selected[feature].name}
-                            onChange={e => this.props.handleSelection(feature, item)}/>
-                        <label htmlFor={itemHash} className="feature__label">
-                            {item.name} ({USCurrencyFormat.format(item.cost)})
-                        </label>
-                    </div>
+                    <input
+                        type="radio"
+                        id={itemHash}
+                        className="feature__option"
+                        name={slugify(feature)}
+                        checked={item.name === selected[feature].name}
+                        onChange={e => this.props.handleSelection(feature, item)}/>
+                    <label htmlFor={itemHash} className="feature__label">
+                        {item.name} ({USCurrencyFormat.format(item.cost)})
+                    </label>
+                </div>
                 );
             });
 
-            return options
+            return (
+                <fieldset className="feature" key={featureHash}>
+                    <legend className="feature__name">
+                        <h3>{feature}</h3>
+                    </legend>
+                    {options}
+                </fieldset>
+            );
         });
 
         return allFeatures
